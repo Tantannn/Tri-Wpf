ad744dad204d741ede0846d37a83494e74d37dd2
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-06-22 18:16:45 +0700

    update code

---
+++ Tri-Wpf/Views/ReceiveBeamTab/ReceiveBeam.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Controls;
+
+namespace Tri_Wpf.Views.ReceiveBeamTab;
+
+public partial class ReceiveBeam : UserControl
+{
+    public ReceiveBeam()
+    {
+        InitializeComponent();
+    }
+}
