89218372bf0a2ae6a32b721b06d1d97508d4c07f
Nm-Tan <47451965+Tantannn@users.noreply.github.com>
2025-07-16 22:01:42 +0700

    update coede

--- Tri-Wpf/ViewModels/ReceiveBeamVm/ReceiveBeamVm.cs
+++ Tri-Wpf/ViewModels/ReceiveBeamVm/ReceiveBeamVm.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Windows.Input;
+using Tri_Wpf.Commands;
 using Tri_Wpf.Core;
 using Tri_Wpf.Models;
 
@@ -8,24 +9,45 @@ namespace Tri_Wpf.ViewModels.ReceiveBeamVm;
 public class ReceiveBeamVm : BaseViewModel
 {
     public ICommand GirderStepCommand { set; get; }
+    public ICommand AddCommand { get; }
 
     public ReceiveBeamVm()
     {
-        // GirderSteps = girderSteps;
-        // GirderStepCommand = girderStepCommand;
-
         GirderSteps = new List<GirderStepItem>();
 
-        // Add 4 sample items (last one will be disabled)
+        // Initialize commands
+        AddCommand = new RelayCommand(_ => AddItem());
+
+        // Add sample data
         for (int i = 1; i <= 4; i++)
         {
             var item = new GirderStepItem
             {
                 Step = i,
                 PillarMaterial = $"Material-{i}",
-                IsLastRow = (i == 4) // Mark last item
+                IsLastRow = (i == 4)
             };
             GirderSteps.Add(item);
+        }
+    }
+
+    private void AddItem()
+    {
+        var newItem = new GirderStepItem
+        {
+            Step = GirderSteps.Count + 1,
+            PillarMaterial = "New Material"
+        };
+        GirderSteps.Add(newItem);
+        UpdateStepNumbers();
+    }
+
+    private void UpdateStepNumbers()
+    {
+        for (int i = 0; i < GirderSteps.Count; i++)
+        {
+            GirderSteps[i].Step = i + 1;
+            GirderSteps[i].IsLastRow = (i == GirderSteps.Count - 1);
         }
     }
 
